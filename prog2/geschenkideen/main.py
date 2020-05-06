e7ecf4b54be30401587786ef14a66cc14784b9fa
melsika <61312058+melsika@users.noreply.github.com>
2020-05-06 22:35:40 +0200

    Dateistruktur html und erste Bootstrap Varianten

--- prog2/geschenkideen/main.py
+++ prog2/geschenkideen/main.py
@@ -1,1 +1,43 @@
-main
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask("Geschenkidee")
+
+"""
+@app.route('/hello/')
+@app.route("/hello/<name>")
+def begruessung(name=False):
+    if name:
+        return "Hallo " + name + "!"
+    else:
+        return "Not Hallo World again…"
+
+"""
+
+
+
+@app.route("/hello/", methods=['GET', 'POST'])
+def hallo():
+    if request.method == 'POST':
+        ziel_person = request.form['vorname']
+        rueckgabe_string = "Hello " + ziel_person + "!"
+        return rueckgabe_string
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+"""
+
+if __nachname__ == "__main__":    
+	app.run(debug=True, port=5000)
+
+if __strasse__ == "__main__":    
+	app.run(debug=True, port=5000)
+
+if __zahl__ == "__main__":    
+	app.run(debug=True, port=5000)
+"""
