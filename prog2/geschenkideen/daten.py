44e637732158df1cb37faad9c3b647aadb22b90a
melsika <61312058+melsika@users.noreply.github.com>
2020-05-24 21:51:02 +0200

    formular suche angefangen

--- prog2/geschenkideen/daten.py
+++ prog2/geschenkideen/daten.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+
 import json
+
+# Das sind meine allgemeinen Speichern- und Lesefunktionen von Json Dateien
 def json_speichern(datei_name, datei_inhalt):
     with open(datei_name, "w") as open_file:
         json.dump(datei_inhalt, open_file, indent=4)
@@ -11,21 +13,42 @@ def json_lesen(datei_name):
     except Exception:
         return []
 
+
+"""
+Einer Person kann nicht 2x die gleiche Geschenkidee hinterlegt werden, 
+dazu vergleiche ich den namen und die idee.
+Ich gehe am Anfang davon aus, dass die Idee noch nicht vorhanden ist, 
+deshalb False, dann prüfe ich und gebe zurück, dass das nicht möglich ist.
+"""
 def idee_bereits_vorhanden(personen_name, idee):
     inhalt = json_lesen("geschenk_idee_formular.json")
     bereits_vorhanden = False
     for eintrag in inhalt:
         if eintrag["person"]== personen_name and eintrag["idee"] == idee:
             bereits_vorhanden = True
-    return bereits_vorhanden
+    return bereits_vorhanden 
+    #WIESO hier bereits_vorhanden? im return?
             
 
+"""
+In meine leere resultat Liste werden die gesuchten Personen hinzugefügt 
+"""
 def geschenke_anzeigen_fuer_person(personen_name):
+    inhalt = json_lesen("geschenk_idee_formular.json")
+    resultat_person = []
+    for eintrag in inhalt:
+        if eintrag["person"]== personen_name:
+            resultat_person.append(eintrag)
+    return resultat_person
+
+
+
+"""
+Hier suche ich nach allen Einträgen, die diesen hashtag geispeichert haben.
+"""
+def hashtags_anzeigen():
     inhalt = json_lesen("geschenk_idee_formular.json")
     resultat = []
     for eintrag in inhalt:
-        if eintrag["person"]== personen_name:
-            resultat.append(eintrag)
-    return resultat
-    
- 
+        resultat.append(eintrag["hashtag"])
+    return list(set(resultat))
--- prog2/geschenkideen/main.py
+++ prog2/geschenkideen/main.py
@@ -1,36 +1,24 @@
 
 from flask import Flask
 from flask import request
+from flask import redirect
 from flask import render_template
 from json import loads, dumps
-import daten
-from datetime import datetime
 import json
+import daten
 
 
 app = Flask("Geschenkidee")
 app = Flask("Daten")
 app = Flask("templates")
 
-geschenk_idee_json = "./geschenk_idee.json"
 
-"""
-@app.route('/hello/')
-@app.route("/hello/<name>")
-def begruessung(name=False):
-    if name:
-        return "Hallo " + name + "!"
-    else:
-        return "Not Hallo World again…"
-
-"""
 
 """alle Seitenverlinkungen"""
 
 @app.route('/geschenkidee')
 def home():
-    geschenk = "hallotestgeklappt"
-    return render_template('index.html', name="Melanie", geschenk="geklappt" )
+    return render_template('index.html')
 
 
 @app.route("/geschenk_erfassen")
@@ -39,77 +27,113 @@ def geschenk_erfassen():
 
 
 
-@app.route("/geschenkidee_suchen")
+@app.route("/geschenkidee_suchen/")
 def geschenk_suchen():
     return render_template('geschenk_suchen.html')
+
 
 
 @app.route("/ergebnis/<personen_name>")
 def ergebnis(personen_name):
     inhalt = daten.geschenke_anzeigen_fuer_person(personen_name)
-    return render_template('ergebnis.html', geschenk_ideen= inhalt)
+    return render_template('ergebnis_person.html', geschenk_ideen= inhalt)
 
 
-@app.route("/geschenk_idee")
-def index():
-    return "Willkommen zu unsere App."
 
-"""--------------------------------------------"""
-"""json mit Internet"""
+@app.route("/hashtags")
+def hashtags():
+    resultat = {
+    "test1key" : daten.hashtags_anzeigen()
+    }
+    return render_template('ergebnis_hashtag.html', resultat=resultat)
+
 """
-try:
-    open_file = open("text.txt", encoding = 'utf-8')
-except Exception as f:
-    raise
-else:
-    pass
-finally:
-    open_file.close()
+@app.route("/ergebnis/<personen_name>")
+def ergebnis(personen_name):
+    inhalt = daten.geschenke_anzeigen_fuer_person(personen_name)
+    return render_template('ergebnis_person.html', geschenk_ideen= inhalt)
 
 
-def doku_test_schreiben():
-    with open('text.txt','w', encoding = 'utf-8') as open_file:
-        open_file.write("erstes dokun")
-        open_file.write("zweite zeile")
-        open_file.write("letzte")
 
-def doku_test_lesen():
-    open_file = open("text.txt", 'r', encoding = 'utf-8')
-    open_file.read(4)
-
-
-@app.route("/json_test")
-def json_test():
-    
-    with open('geschenk_idee.json') as open_file:
-        data = json.load(                                     open)
-    return print(data)
-
-
-@app.route("/speichern_ich")
-def json_speichern():
-    geschenk_idee = [{'name': 'Maja', 'tel': 123}]
-
-    with open ('geschenk_idee_1.json', 'w') as open_file:
-        json_als_string = dumps(geschenk_idee)
-        open_file.write(json_als_string)
-
-    return render_template("ergebnis.html", liste = geschenk_idee)
-    
-
-@app.route("/einlesen")
-def json_einlesen():
-    with open('geschenk_idee.json') as open_file:
-        json_als_string = open_file.read()
-        mein_eingelesenes_dict = loads(json_als_string)
-
-    return render_template("ergebnis.html", mein_eingelesenes_dict)
-
+@app.route("/hashtags")
+def hashtags():
+    resultat = {
+    "test1key" : daten.hashtags_anzeigen()
+    }
+    return resultat
 """
 
 """---------------------------------------------------"""
 
-"""Loris"""
+
+"""
+Beim Formular nehme ich die neuen Einträge mit request entgegen und forme direkt das json
+Jede Idee kann nur einmal vorkommen, ich rufe die Hilfsfunktion dazu im daten.py auf.
+Der json_speichern funktion gebe ich als variable an welches dokument und die andere variable ist der Inhalt,
+in diese habe ich zuvor den aktuellen Inhalt geladen und den neuen Eintrag hinzugefügt, so wird der neue Inhalt nun gespeichert.
+"""
+  
+@app.route("/geschenk_erfassen", methods=['GET', 'POST'])
+def formular_neuer_eintrag():
+    if request.method == 'POST':
+        idee = request.form["idee"]
+        beschreibung = request.form["beschreibung"]
+        person = request.form["person"]
+        hashtag = request.form["hashtag"]
+        inhalt = daten.json_lesen("geschenk_idee_formular.json")
+        neuer_eintrag = {
+            "person" : person,
+            "idee" : idee,
+            "beschreibung" : beschreibung,
+            "hashtag" : hashtag
+        }
+        if daten.idee_bereits_vorhanden(person, idee):
+            return "Eintrag bereits vorhanden, da kann ich noch was anderes"
+        inhalt.append(neuer_eintrag)
+        daten.json_speichern("geschenk_idee_formular.json", inhalt)
+        return "speichern erfolgreich"
+
+    return render_template("geschenk_erfassen.html")
+
+
+@app.route("/geschenkidee_suchen/", methods=['GET', 'POST'])
+def formular_eintrag_suchen():
+    if request.method == 'POST':
+        personen_name = request.form["person"]
+        return personen_name
+
+"""
+@app.route("/hello/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        ziel_person = request.form['vorname']
+        rueckgabe_string = "Hello " + ziel_person + "!"
+        return rueckgabe_string
+
+    return render_template("index.html")
+  """ 
+"""
+
+@app.route("/ergebnis/<personen_name>")
+def ergebnis(personen_name):
+    """
+    
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+"""
+
+if __nachname__ == "__main__":    
+	app.run(debug=True, port=5000)
+
+if __strasse__ == "__main__":    
+	app.run(debug=True, port=5000)
+
+if __zahl__ == "__main__":    
+	app.run(debug=True, port=5000)
+"""
+
+"""Loris
 def eintrag_speichern(pd, id, datum, symp1, symp2, symp3, symp4, symp5, symp6, symp7, symp8):
     pd = dic_laden()
     if id in pd:
@@ -153,76 +177,4 @@ def eintrag_speichern(pd, id, datum, symp1, symp2, symp3, symp4, symp5, symp6, s
         pd[id] = {datum: {"Sodbrennen": symp1, "Rückfluss von Flüssigkeit oder Nahrung in den Mund": symp2, "Schmerzen im Oberbauch": symp3, "Beschwerden beim Schlucken der Nahrung": symp4, "Heiserkeit": symp5, "Halsschmerzen": symp6, "Mundbrennen": symp7, "Völlegefühl im Oberbauch": symp8}}
     with open("./templates/json/test.json", "w", encoding='utf-8') as open_file:
             json.dump(pd, open_file, indent = 4)
-
-"""______--------------------------------------------------"""
-
-
-"""Odoni Github"""
-
-
-  
-@app.route("/geschenk_erfassen", methods=['GET', 'POST'])
-def formular_neuer_eintrag():
-    if request.method == 'POST':
-        idee = request.form["idee"]
-        beschreibung = request.form["beschreibung"]
-        person = request.form["person"]
-        hashtag = request.form["hashtag"]
-        inhalt = daten.json_lesen("geschenk_idee_formular.json")
-        neuer_eintrag = {
-            "person" : person,
-            "idee" : idee,
-            "beschreibung" : beschreibung,
-            "hashtag" : hashtag
-        }
-        if daten.idee_bereits_vorhanden(person, idee):
-            return "Eintrag bereits vorhanden, da kann ich noch was anderes"
-        inhalt.append(neuer_eintrag)
-        daten.json_speichern("geschenk_idee_formular.json", inhalt)
-        return "speichern erfolgreich"
-
-    return render_template("geschenk_erfassen.html")
-
-   
-
-
-@app.route("/liste")
-def auflisten():
-    formular = daten.formular_laden()
-
-    formular_liste = ""
-    for key, value in geschenk_idee_formular.items():
-        zeile = str(key) + ": " + value + "<br>"
-        formular_liste += zeile
-
-    return formular_liste
-
-
-
-
-
-
-"""
-@app.route("/helloo/", methods=['GET', 'POST'])
-def hallo():
-    if request.method == 'POST':
-        ziel_person = request.form['vorname']
-        rueckgabe_string = "Hello " + ziel_person + "!"
-        return rueckgabe_string
-
-    return render_template("index.html")
-"""
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
-"""
-
-if __nachname__ == "__main__":    
-	app.run(debug=True, port=5000)
-
-if __strasse__ == "__main__":    
-	app.run(debug=True, port=5000)
-
-if __zahl__ == "__main__":    
-	app.run(debug=True, port=5000)
 """
